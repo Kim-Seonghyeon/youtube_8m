1398d444710cf06c53b9a84da0a7c2d62fb2c605
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-02 18:07:26 +0900

    Update train.py

--- train.py
+++ train.py
@@ -39,12 +39,12 @@ if __name__ == "__main__":
   flags.DEFINE_string("train_dir", "/tmp/yt8m_train",
                       "The directory to save the model files in.")
   flags.DEFINE_string(
-      "train_data_pattern", 'gs://youtube8m-ml/1/video_level/train/train*.tfrecord',
+      "train_data_pattern", 'gs://youtube8m-ml/1/frame_level/train/train*.tfrecord',
       "File glob for the training dataset. If the files refer to Frame Level "
       "features (i.e. tensorflow.SequenceExample), then set --reader_type "
       "format. The (Sequence)Examples are expected to have 'rgb' byte array "
       "sequence feature as well as a 'labels' int64 context feature.")
-  flags.DEFINE_string("feature_names", "mean_rgb", "Name of the feature "
+  flags.DEFINE_string("feature_names", "rgb", "Name of the feature "
                       "to use for training.")
   flags.DEFINE_string("feature_sizes", "1024", "Length of the feature vectors.")
 
