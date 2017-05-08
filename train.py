149c2202e84074a3806f807abe2e17e29b7d5de6
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-08 14:54:11 +0900

    Update train.py

--- train.py
+++ train.py
@@ -36,10 +36,10 @@ FLAGS = flags.FLAGS
 
 if __name__ == "__main__":
   # Dataset flags.
-  flags.DEFINE_string("train_dir", 'IDEA/seminar/youtube/youtube_8m',
+  flags.DEFINE_string("train_dir", '/home/ksh/IDEA/seminar/youtube/youtube_8m',
                       "The directory to save the model files in.")
   flags.DEFINE_string(
-      "train_data_pattern", '~/data/yt8m/trian*.tfrecord',
+      "train_data_pattern", '/home/ksh/data/yt8m/trian*.tfrecord',
       "File glob for the training dataset. If the files refer to Frame Level "
       "features (i.e. tensorflow.SequenceExample), then set --reader_type "
       "format. The (Sequence)Examples are expected to have 'rgb' byte array "
