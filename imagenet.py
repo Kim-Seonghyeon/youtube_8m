76cf570390aaa9d1b5b69624a8bad748949c45d3
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-13 06:59:38 +0900

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -24,12 +24,7 @@ out_file.write("filename," + ",".join(["feature" + str(i) for i in range(1, 2049
 for i in range(len(files)):
     image_data = tf.gfile.FastGFile(files[i], 'rb').read()
 
-    # 몇가지 유용한 텐서들:
-    # 'softmax:0': 1000개의 레이블에 대한 정규화된 예측결과값(normalized prediction)을 포함하고 있는 텐서
-    # 'pool_3:0': 2048개의 이미지에 대한 float 묘사를 포함하고 있는 next-to-last layer를 포함하고 있는 텐서
-    # 'DecodeJpeg/contents:0': 제공된 이미지의 JPEG 인코딩 문자를 포함하고 있는 텐서
-
-    # image_data를 인풋으로 graph에 집어넣고 softmax tesnor를 실행한다.
+    logging.info(i)
     feature = sess.run(softmax_tensor,
                            {'DecodeJpeg/contents:0': image_data})
     feature = np.squeeze(feature)
