d9567cb571115c87698717d31b26316a795a12e7
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-13 01:38:40 +0900

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -11,11 +11,15 @@ VGG_MEAN = [103.939, 116.779, 123.68]
 
 class Vgg16:
     def __init__(self, vgg16_npy_path=None):
+        if vgg16_npy_path is None:
 
-            
+            path = "gs://ksh_imagenet/vgg16/vgg16.npy"
+            vgg16_npy_path = path
+            print(path)
 
+        self.data_dict = np.load(vgg16_npy_path).item()
+        print("npy file loaded")
 
-        print "npy file loaded"
 
     def build(self, rgb):
         """
