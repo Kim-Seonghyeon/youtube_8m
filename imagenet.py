c9713e17ebf0b096dbd3c53207657d1d9027f768
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-13 12:48:04 +0900

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -6,15 +6,16 @@ import tensorflow as tf
 import time
 from tensorflow import logging
 
+f=tf.gfile.FastGFile("gs://ksh_imagenet/vgg16/classify_image_graph_def.pb", 'rb')
+graph_def = tf.GraphDef()
+graph_def.ParseFromString(f.read())
+_ = tf.import_graph_def(graph_def, name='')
+
 config = tf.ConfigProto(allow_soft_placement = True)
 sess = tf.Session(config = config)
 with tf.device("/gpu:0"):
-    f=tf.gfile.FastGFile("gs://ksh_imagenet/vgg16/classify_image_graph_def.pb", 'rb')
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    _ = tf.import_graph_def(graph_def, name='')
 
-    softmax_tensor = sess.graph.get_tensor_by_name('pool_3:0')
+    pool_3_tensor = sess.graph.get_tensor_by_name('pool_3:0')
     files = tf.gfile.Glob("gs://ksh_imagenet/ILSVRC/Data/DET/test/*.JPEG")
     logging.info(files)
     out_file = gfile.Open("gs://ksh_imagenet/ILSVRC/feature.csv", "w+")
@@ -25,7 +26,7 @@ with tf.device("/gpu:0"):
         image_data = tf.gfile.FastGFile(files[i], 'rb').read()
 
         logging.info(i)
-        feature = sess.run(softmax_tensor,
+        feature = sess.run(pool_3_tensor,
                                {'DecodeJpeg/contents:0': image_data})
         feature = np.squeeze(feature)
         out_file.write(files[i] + "," + ",".join(["%f" % y for y in feature]) + "\n")
