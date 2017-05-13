54a30d4f7d8c418c53d437e3e1a5f5f4c0667144
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-13 13:22:38 +0900

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -6,6 +6,10 @@ import tensorflow as tf
 import time
 from tensorflow import logging
 
+out_file = gfile.Open("gs://ksh_imagenet/ILSVRC/feature.csv", "w+")
+out_file.write("filename," + ",".join(["feature" + str(i) for i in range(1, 2049)]) + "\n")
+files = tf.gfile.Glob("gs://ksh_imagenet/ILSVRC/Data/DET/test/*.JPEG")
+logging.info(files)
 f=tf.gfile.FastGFile("gs://ksh_imagenet/vgg16/classify_image_graph_def.pb", 'rb')
 graph_def = tf.GraphDef()
 graph_def.ParseFromString(f.read())
@@ -13,22 +17,19 @@ _ = tf.import_graph_def(graph_def, name='')
 
 config = tf.ConfigProto(allow_soft_placement = True)
 sess = tf.Session(config = config)
-with tf.device("/gpu:0"):
+for k in range(1):
+    with tf.device("/gpu:%d" % k):
+        pool_3_tensor = sess.graph.get_tensor_by_name('pool_3:0')
 
-    pool_3_tensor = sess.graph.get_tensor_by_name('pool_3:0')
-    files = tf.gfile.Glob("gs://ksh_imagenet/ILSVRC/Data/DET/test/*.JPEG")
-    logging.info(files)
-    out_file = gfile.Open("gs://ksh_imagenet/ILSVRC/feature.csv", "w+")
-    out_file.write("filename," + ",".join(["feature" + str(i) for i in range(1, 2049)]) + "\n")
+        for i in range(len(files)):
+            image_data = tf.gfile.FastGFile(files[i], 'rb').read()
 
-
-    for i in range(len(files)):
-        image_data = tf.gfile.FastGFile(files[i], 'rb').read()
-
-        logging.info(i)
-        feature = sess.run(pool_3_tensor,
-                               {'DecodeJpeg/contents:0': image_data})
-        feature = np.squeeze(feature)
-        out_file.write(files[i] + "," + ",".join(["%f" % y for y in feature]) + "\n")
-    out_file.close()
+            logging.info(i)
+            feature = sess.run(pool_3_tensor,
+                                   {'DecodeJpeg/contents:0': image_data})
+            feature = np.squeeze(feature)
+            out_file.write(files[i] + "," + ",".join(["%f" % y for y in feature]) + "\n")
+            
+            
+out_file.close()
 
