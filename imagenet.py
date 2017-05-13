f07b38af87d69fb14e335c5ee8efc557f3be063a
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-13 11:47:00 +0900

    Update imagenet.py

--- imagenet.py
+++ imagenet.py
@@ -7,13 +7,13 @@ import time
 from tensorflow import logging
 
 
-
+sess = tf.Session()
 with tf.device("/gpu:0"):
     f=tf.gfile.FastGFile("gs://ksh_imagenet/vgg16/classify_image_graph_def.pb", 'rb')
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
-    sess = tf.Session()
+
     softmax_tensor = sess.graph.get_tensor_by_name('pool_3:0')
     files = tf.gfile.Glob("gs://ksh_imagenet/ILSVRC/Data/DET/test/*.JPEG")
     logging.info(files)
@@ -30,3 +30,4 @@ with tf.device("/gpu:0"):
         feature = np.squeeze(feature)
         out_file.write(files[i] + "," + ",".join(["%f" % y for y in feature]) + "\n")
     out_file.close()
+
