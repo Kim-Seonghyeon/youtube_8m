d591c37ebde5267cac384f484cf9128f86ad55b1
Kim-Seonghyeon <shkim93@snu.ac.kr>
2017-05-27 10:37:43 +0900

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -69,6 +69,26 @@ def SampleRandomFrames(model_input, num_frames, num_samples):
   index = tf.stack([batch_index, frame_index], 2)
   return tf.gather_nd(model_input, index)
 
+def EqualSpaceFrames(model_input, num_frames, num_samples):
+  """Samples a random set of frames of size num_samples.
+
+  Args:
+    model_input: A tensor of size batch_size x max_frames x feature_size
+    num_frames: A tensor of size batch_size x 1
+    num_samples: A scalar
+
+  Returns:
+    `model_input`: A tensor of size batch_size x num_samples x feature_size
+  """
+  batch_size = tf.shape(model_input)[0]
+  frame_index = tf.cast(tf.tile(tf.expand_dims(tf.range(num_samples - 1), 0), [batch_size, 1]) / (num_samples - 1), tf.float32) * tf.cast(tf.tile(num_frames, [1, num_samples - 1]), tf.float32)
+  frame_index = tf.cast(frame_index, tf.int32)
+  frame_index = tf.concat([frame_index, num_frames - 1], 1)
+  batch_index = tf.tile(
+      tf.expand_dims(tf.range(batch_size), 1), [1, num_samples])
+  index = tf.stack([batch_index, frame_index], 2)
+  return tf.gather_nd(model_input, index)
+
 def FramePooling(frames, method, **unused_params):
   """Pools over the frames of a video.
 
